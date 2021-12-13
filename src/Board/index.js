ed2b2a6e0554f8c075f12d040321a05a8881dce9
J.S <jimmy.sene@gmail.com>
2021-12-13 18:20:43 +0100

    grid done

--- src/Board/index.js
+++ src/Board/index.js
@@ -1,10 +1,28 @@
 import './style.scss';
 import Square from '../Square';
+import { Component } from 'react';
 
-export default function Board() {
-  return (
-    <div className="board">
-      <Square />
-    </div>
-  )
+export default class Board extends Component {
+
+  state = {
+    // 8 squares in board
+    squares: Array.from(Array(9).keys()).map(e => <Square value={e} key={e} />)
+  }
+
+  render() {
+    return (
+      <div className="board">
+        <div className="board__row">
+          {this.state.squares.slice(0, 3)}
+        </div>
+        <div className="board__row">
+          {this.state.squares.slice(3, 6)}
+        </div>
+        <div className="board__row">
+          {this.state.squares.slice(6, 9)}
+        </div>
+      </div>
+    )
+  }
+
 }
