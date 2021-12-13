8a8caf88ca7191d74548aedb1571dbcb770a3765
J.S <jimmy.sene@gmail.com>
2021-12-13 20:31:29 +0100

    Readme updated

--- src/Game/index.js
+++ src/Game/index.js
@@ -1,15 +1,12 @@
 import './style.scss';
 import Board from '../Board';
-import { Component } from 'react';
 
-export default class Game extends Component {
-
-  render() {
-    return (
-      <div className="game">
-        <h1>Morpion avec React v17</h1>
-        <Board />
-      </div>
-    )
-  }
+export default function Game() {
+  return (
+    <div className="game">
+      <h1>Morpion avec React v17</h1>
+      <Board />
+    </div>
+  )
 }
+
